745d449bf806c5c0461c4439cbb526071a0d2add
siddharth111 <siddharth.cn@aurigait.com>
2015-07-16 14:07:46 +0530

    second commit

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ var express = require('express') ,
 	path 	= require('path');
 
 app	
-	.use(express.static('./public'))
+	.use(express.static(path.join(__dirname,'/public')))
 	.use(express.static(path.join(__dirname,'bower_components')))
 	.get('*' , function (req , res) {
 		res.sendfile('public/index.html');
 	});
-var port = Number(process.env.PORT || 3000);
-	app.listen(port);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
